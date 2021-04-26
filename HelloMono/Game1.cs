c16b0d7af5173931f24cf39df61bc0eda44d02a2
Cuber01 <johnmallard631@gmail.com>
2021-04-26 17:50:09 +0200

    added some files

--- HelloMono/Game1.cs
+++ HelloMono/Game1.cs
@@ -13,15 +13,23 @@ namespace HelloMono
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+          base.Initialize();
+          
+          Window.Title = "My First Game";
+          this.IsMouseVisible = true;
+          Window.AllowUserResizing = true;
 
-            base.Initialize();
+          _graphics.PreferredBackBufferWidth = 600;
+          _graphics.PreferredBackBufferHeight = 500;   
+          _graphics.ApplyChanges();
+          
         }
+        
 
         protected override void LoadContent()
         {
@@ -32,7 +40,8 @@ namespace HelloMono
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
@@ -42,10 +51,21 @@ namespace HelloMono
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+            
+            _spriteBatch.Begin();
+            
+                Texture2D rect = new Texture2D(_graphics.GraphicsDevice, 80, 30);
 
-            // TODO: Add your drawing code here
+                Color[] data = new Color[80*30];
+                for(int i=0; i < data.Length; ++i) data[i] = Color.Chocolate;
+                rect.SetData(data);
 
+                Vector2 coor = new Vector2(10, 20);
+                _spriteBatch.Draw(rect, coor, Color.White);
+
+            _spriteBatch.End();
+            
             base.Draw(gameTime);
         }
     }
